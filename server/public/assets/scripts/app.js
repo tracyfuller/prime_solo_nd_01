34ffa10c1d16d4c2ceeb46be864467edc412d28c
tracyfuller <tracy.e.fuller@gmail.com>
2015-05-07 18:43:14 -0500

    fix error of javascript not loading correctly

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,16 +1,1 @@
-$document.ready(function(){
-    $('#myTab a[href="#about"]').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    });
-    $('#myTab a[href="#forgeLake"]').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    });
-    $('#myTab a[href="#bitsMonster"]').click(function (e) {
-        e.preventDefault()
-        $(this).tab('show')
-    });
-    console.log("javascript is loading.");
-
-});
+console.log("javascript is loading.");
