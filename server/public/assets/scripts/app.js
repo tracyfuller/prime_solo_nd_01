2f54d73b70bd9db9bb910e6b34fa5714e0152d25
tracyfuller <tracy.e.fuller@gmail.com>
2015-05-07 23:26:27 -0500

    layout product slide for shop tabs. Fixed layout issues and bugs

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -1,1 +1,8 @@
 console.log("javascript is loading.");
+$(document).ready(function(){
+
+    $('.product').on('click', function(){
+       $(this).siblings('#slideCont').slideToggle('slow');
+       console.log("label clicked");
+   });
+});
