d9188ba1f8c4a0a7bccb63cc1f12fb2b385d7e1a
tracyfuller <tracy.e.fuller@gmail.com>
2015-05-08 11:05:56 -0500

    Profile picture now round, tab and content color fix

--- server/public/assets/scripts/app.js
+++ server/public/assets/scripts/app.js
@@ -2,7 +2,7 @@ console.log("javascript is loading.");
 $(document).ready(function(){
 
     $('.product').on('click', function(){
-       $(this).siblings('#slideCont').slideToggle('slow');
+       $(this).siblings('.slideCont').slideToggle('slow');
        console.log("label clicked");
    });
 });
